ef605f24e60cead42c8c040f872a6a352107136d
Rafael <rafavil@gmail.com>
2016-06-15 16:45:54 +0000

    Added edit, update & destroy actions

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,6 @@
 class ContactsController < ApplicationController
+  before_action :find_contact, only: [:edit, :update, :destroy]
+  
   def index
     @contacts = Contact.all
   end
@@ -17,7 +19,30 @@ class ContactsController < ApplicationController
     end
   end
   
+  def edit
+    
+  end
+  
+  def update
+    if @contact.update(contact_params)
+      flash[:success] = "The contact was updated!"
+      redirect_to contacts_path
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @contact.destroy
+    flash[:success] = "The contact was deleted!"
+    redirect_to contacts_path
+  end
+  
   private
+  
+  def find_contact
+    @contact = Contact.find(params[:id])
+  end
   
   def contact_params
     params.require(:contact).permit(:name, :company, :email, :phone, :address)
